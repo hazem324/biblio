5bd5c792a91ce45f8d10447ac91bf5e8f57f3c86
Hazem Hadda <hazemhadda2002@gmail.com>
2024-10-17 01:15:33 +0100

    crud

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -5,10 +5,12 @@ namespace App\Controller;
 use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\AuthorRepository;
 use App\Entity\Author;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Form\PersonneType;
 
 class AuthorController extends AbstractController
 {
@@ -28,4 +30,51 @@ class AuthorController extends AbstractController
         
         return $this->redirectToRoute('app_author');
     }
+
+    #[Route('/add-auth', name: 'app_addauthors')]
+    public function addAuth(ManagerRegistry $m,Request $req): Response
+    {
+        $em=$m->getManager();
+        $author=new Author();
+        $form=$this->createForm(PersonneType::class,$author);
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
+        $em->persist($author);
+        $em->flush();
+        return $this->redirectToRoute('app_author');
+            }
+        return $this->render('author/addauthor.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/edit-auth/{id}', name: 'app_updateformauthors')]
+    public function editAuth(ManagerRegistry $m,Request $req,$id,AuthorRepository $rep): Response
+    {
+        $em=$m->getManager();
+        $author=$rep->find($id);
+        $form=$this->createForm(PersonneType::class,$author);
+        $form->handleRequest($req);
+        if($form->isSubmitted() && $form->isValid()){
+        $em->persist($author);
+        $em->flush();
+        return $this->redirectToRoute('app_author');
+            }
+        return $this->render('author/addauthor.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+
+    #[Route('/delete-auth/{id}', name: 'app_deleteauthors')]
+    public function deleteAuth(ManagerRegistry $m,Request $req,$id,AuthorRepository $rep): Response
+    {$em=$m->getManager();
+        $author=$rep->find($id); 
+        $em->remove($author);
+        $em->flush();
+        return $this->redirectToRoute('app_author');
+            
+    }
 }
+
